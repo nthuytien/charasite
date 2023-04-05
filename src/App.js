2f66ecf98f11b5abf866a1eefe999b59d6d604f2
nthuytien <nthuytien35@gmail.com>
2023-04-05 02:30:20 -0400

    make it pwetty

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="body">
+      <br/><br/>
+        <div className="header-image">
+          <div className='font-face'>
+          CyberPunk Red
+          </div>
+        </div>
+        <br/><br/>
+      An Apocalyptic Cyberpunk TTRPG Game
+      <hr/><br/>
+
+      <div className="blurb">
+      insert blurb here. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Libero enim sed faucibus turpis in eu mi bibendum neque. Ullamcorper sit amet risus nullam eget felis eget nunc. Vulputate eu scelerisque felis imperdiet proin fermentum leo vel. Vestibulum morbi blandit cursus risus at ultrices mi. Amet aliquam id diam maecenas ultricies mi eget. Aenean et tortor at risus viverra adipiscing at in tellus. Feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi. Tortor condimentum lacinia quis vel. Tempor id eu nisl nunc mi ipsum. Id faucibus nisl tincidunt eget nullam non nisi est. Ac tortor vitae purus faucibus. Adipiscing diam donec adipiscing tristique. Interdum velit euismod in pellentesque massa placerat duis ultricies lacus. Laoreet suspendisse interdum consectetur libero id faucibus nisl tincidunt. Lobortis elementum nibh tellus molestie. A iaculis at erat pellentesque adipiscing commodo elit at. Id neque aliquam vestibulum morbi blandit cursus risus at ultrices.
+
+Nibh ipsum consequat nisl vel pretium. Nulla posuere sollicitudin aliquam ultrices sagittis orci a scelerisque purus. Morbi tristique senectus et netus et malesuada fames ac turpis. Nunc sed augue lacus viverra vitae. Id nibh tortor id aliquet lectus. Lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit amet. Sed arcu non odio euismod lacinia. Commodo odio aenean sed adipiscing diam. Turpis egestas pretium aenean pharetra magna. Sit amet mauris commodo quis. Convallis convallis tellus id interdum velit.
+    </div>
+    <br/>
+      <div className='button'>
+
+        <button>Rulebook</button>  <button>NPC Characters</button>  <button>Player Characters</button>
+      </div>
+
+      <br/><br/>
+      <div class="row"><div class="column">
+      <img class="imageN" src= {require('./net.png')}/> 
+      </div>
+      <div class="column">
+      Diverse Characters 
+      <br/> <br/>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Libero enim sed faucibus turpis in eu mi bibendum neque. Ullamcorper sit amet risus nullam eget felis eget nunc. Vulputate eu scelerisque felis imperdiet proin fermentum leo vel. Vestibulum morbi blandit cursus risus at ultrices mi. Amet aliquam id diam maecenas ultricies mi eget. Aenean et tortor at risus viverra adipiscing at in tellus. Feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi. Tortor condimentum lacinia quis vel. Tempor id eu nisl nunc mi ipsum. Id faucibus nisl tincidunt eget nullam non nisi est. Ac tortor vitae purus faucibus. Adipiscing diam donec adipiscing tristique. Interdum velit euismod in pellentesque massa placerat duis ultricies lacus. Laoreet suspendisse interdum consectetur libero id faucibus nisl tincidunt. Lobortis elementum nibh tellus molestie. A iaculis at erat pellentesque adipiscing commodo elit at. Id neque aliquam vestibulum morbi blandit cursus risus at ultrices.
+      </div>
+      <div class="column">
+      <img class="imageN" src= {require('./hot.png')}/>
+      </div>
+      </div>
+      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+
+      
+      </div>
+        
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './fonts/Robot9000.ttf';
+import './fonts/Orbitron-Medium.ttf';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
