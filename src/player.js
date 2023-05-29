817066c905c1f606e10938168ba8253e505cb5cb
Braeden Wist <braeden.wist.aos@gmail.com>
2023-05-29 17:53:31 -0400

    Changed formatting of character page; removed table element for database records

--- src/player.js
+++ src/player.js
@@ -12,12 +12,20 @@ import {
 //Okay this part kinda confused me but as far as I can tell it's basically just defining the
 //HTML structure for a "Record", which is how we'll process all of our characters.
 const Record = (props) => (
-  <tr>
-    <td>{props.record.name}</td>
-    <td>{props.record.bio}</td>
-    <td>{props.record.age}</td>
-    <td>{props.record.role}</td>
-  </tr>
+
+  <div className="row"><div className="column3">
+    <img className="imageC" src={require('./assets/marcie.png')} />
+  </div>
+    <div className="column2">
+
+      <div className="charaHeader">
+        {props.record.name}
+      </div>
+      {props.record.role} | {props.record.age}
+      <br /> <br />
+      {props.record.bio}
+    </div>
+  </div>
 )
 
 function Chara() {
@@ -44,16 +52,16 @@ function Chara() {
       // The server code is running the DB connection on port 5000 and the record.js file defined
       // this /record/ route as a way to just grab all of our characters as a response.
       const response = await fetch(`http://localhost:5000/record/`);
-  
+
       if (!response.ok) {
         const message = `An error occurred: ${response.statusText}`;
         window.alert(message);
         return;
       }
-      else{
+      else {
         console.log("No error!")
       }
-  
+
       // Now we take the JSON from that response and set our records to that.
       const records = await response.json();
       setRecords(records);
@@ -61,9 +69,9 @@ function Chara() {
       //console.log("Got records!")
       //console.log(records)
     }
-  
+
     getRecords();
-  
+
     return;
   }, [records.length]);
 
@@ -89,7 +97,7 @@ function Chara() {
       </div>
       <div className="body">
         <br /><br />
-        <div className="header-image" style={{backgroundImage: `url(` + require(`${backgroundImageURL}`) + `)`}}>
+        <div className="header-image" style={{ backgroundImage: `url(` + require(`${backgroundImageURL}`) + `)` }}>
           <div className='font-face'>
             PLAYERS
           </div>
@@ -114,18 +122,12 @@ function Chara() {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Libero enim sed faucibus turpis in eu mi bibendum neque. Ullamcorper sit amet risus nullam eget felis eget nunc. Vulputate eu scelerisque felis imperdiet proin fermentum leo vel. Vestibulum morbi blandit cursus risus at ultrices mi. Amet aliquam id diam maecenas ultricies mi eget. Aenean et tortor at risus viverra adipiscing at in tellus. Feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi. Tortor condimentum lacinia quis vel. Tempor id eu nisl nunc mi ipsum. Id faucibus nisl tincidunt eget nullam non nisi est. Ac tortor vitae purus faucibus. Adipiscing diam donec adipiscing tristique. Interdum velit euismod in pellentesque massa placerat duis ultricies lacus. Laoreet suspendisse interdum consectetur libero id faucibus nisl tincidunt. Lobortis elementum nibh tellus molestie. A iaculis at erat pellentesque adipiscing commodo elit at. Id neque aliquam vestibulum morbi blandit cursus risus at ultrices.
           </div>
         </div>
+        {characterList()}
         <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
         {/*--And down here we create a table.
               We call characterList() to use the function we defined earlier
               and create a Record for each character.  It basically just inserts whatever gets returned.*/}
-        <table>
-          <thead>
-            <tr>
-              <th>NAME</th>
-            </tr>
-          </thead>
-          <tbody> {characterList()}</tbody>
-        </table>
+        
 
 
       </div>
